7503b09133ea34a768c14a8f85133ea385694dc6
Wojciech <46322266+Wojtas71@users.noreply.github.com>
2024-03-22 15:30:05 +0100

    adding test m2_test1 and m2_test2

---
+++ cypress/e2e/firstTest.cy.js
@@ -0,0 +1,22 @@
+// describe("Pierwszy blok testów", () => {
+//     it("Wizyta na stronie GoIT", () => {
+//         cy.visit("https://www.edu.goit.global/account/login");
+//     });
+
+// //     it("Wpisz email user888", () => {
+// //         cy.visit("https://www.edu.goit.global/account/login");
+// //         cy.signIn("user888@gmail.com", "1234567890")
+// //     });
+
+// //     it("Wpisz email user888", () => {
+// //         cy.visit("https://www.edu.goit.global/account/login");
+// //         cy.signIn("testowyqa@qa.team ", "QA!automation-1")
+// //     });
+
+// //     it("Testy asercji", () => {
+// //         cy.visit("https://www.edu.goit.global/account/login");
+// //         cy.get('#user_email').scrollIntoView().should("be.visible");
+// //         cy.get('[type="submit"]').should("have.text", "Log in");
+// //         cy.get('[type="submit"]').should("have.css", "background-color", "rgb(255, 107, 10)");
+//     // });
+// });
