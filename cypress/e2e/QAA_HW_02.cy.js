bf4fd41ec389a513cb44dbc7ec22f5b7ec7f15a5
Wojciech <46322266+Wojtas71@users.noreply.github.com>
2024-03-24 22:37:44 +0100

    Update 3.0 QAA_HW_02.cy.js

--- cypress/e2e/QAA_HW_02.cy.js
+++ cypress/e2e/QAA_HW_02.cy.js
@@ -2,10 +2,8 @@
 describe("Testy Moduł 2", () => {
         it("Wpisz email user888", () => {
         cy.visit("https://www.edu.goit.global/account/login");
-        cy.get('#user_email').type('user888@gmail.com');
-        cy.get('#user_password').type('1234567890');
-        cy.get('.eckniwg2').click();
-        cy.wait(5000);
+        cy.login("user888@gmail.com", "1234567890");
+        cy.get('[type="submit"]').should("have.text", "Log in").click();
         cy.get('#open-navigation-menu-mobile').click();
         cy.get(':nth-child(12) > .next-bve2vl').scrollIntoView().should("be.visible");
         cy.wait(5000);
